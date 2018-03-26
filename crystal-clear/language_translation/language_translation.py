d8969eeb79ce142bfdb3d08ccd4203d4bacf2bc1
jdery001 <jdery001@odu.edu>
2018-03-26 17:18:39 -0400

    Removed dictionary variable and get function.

--- crystal-clear/language_translation/language_translation.py
+++ crystal-clear/language_translation/language_translation.py
@@ -10,9 +10,9 @@ class langTranslation:
     #Private variables
     native_Word = ""                # string
     translated_Word = ""            # string
-    definition = ""                 # string
     native_Phrase = ""              # string
     translated_Phrase = ""          # string
+    none_Available = "None available."
 
     inPath = "./translatedWords"   # path of the file containing contents
 
@@ -27,10 +27,9 @@ class langTranslation:
                 temp = line
                 english = temp.partition("$")[0]
                 if native == english.strip('\n'):
-                    self.native_Word,self.translated_Word,self.definition,self.native_Phrase,self.translated_Phrase = temp.split('$')
+                    self.native_Word,self.translated_Word,self.native_Phrase,self.translated_Phrase = temp.split('$')
                     self.native_Word.strip('\n')
                     self.native_Phrase.strip('\n')
-                    self.definition.strip('\n')
                     self.translated_Phrase.strip('\n')
                     self.translated_Word.strip('\n')
                     found = 1
@@ -46,18 +45,12 @@ class langTranslation:
 
     def getNativePhrase (self):
         if self.native_Phrase == "":
-            print ("No phrase available.")
+            return self.none_Available
         else:
             return native_Phrase
 
     def getTranslatedPhrase (self):
         if self.translated_Phrase == "":
-            print ("No phrase available.")
+             return self.none_Available
         else:
             return translated_Phrase
-
-    def getDefinition (self):
-        if self.definition == "":
-            print ("No definition available.")
-        else:
-            return definition
